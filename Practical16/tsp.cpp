ce98e51c6a08459047de86711d1270ce546a7bf7
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2022-11-20 14:43:31 +0530

    Updated TSP

--- Practical16/tsp.cpp
+++ Practical16/tsp.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define V 4
 
 
-int travllingSalesmanProblem(int graph[][V], int s){
+int tsp(int graph[][V], int s){
 
 	vector<int> vertex;
 	for (int i = 0; i < V; i++)
@@ -16,18 +16,18 @@ int travllingSalesmanProblem(int graph[][V], int s){
 	do {
 
 		// storee current Path weeight
-		int currnt_pathweight = 0;
+		int curr_pathweight = 0;
 
 		// compute current path weight
 		int k = s;
 		for (int i = 0; i < vertex.size(); i++) {
-			currnt_pathweight += graph[k][vertex[i]];
+			curr_pathweight += graph[k][vertex[i]];
 			k = vertex[i];
 		}
-		currnt_pathweight += graph[k][s];
+		curr_pathweight += graph[k][s];
 
 		// update minimum
-		min_path = min(min_path, currnt_pathweight);
+		min_path = min(min_path, curr_pathweight);
 
 	} while (
 		next_permutation(vertex.begin(), vertex.end()));
@@ -43,6 +43,6 @@ int main()
 					{ 13, 34, 0, 30 },
 					{ 15, 26, 30, 0 } };
 	int s = 0;
-	cout << travllingSalesmanProblem(graph, s) << endl;
+	cout << tsp(graph, s) << endl;
 	return 0;
 }
